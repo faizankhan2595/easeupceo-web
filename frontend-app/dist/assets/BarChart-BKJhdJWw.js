96498dbb3277a37edc8feff514efbc2e3000c30d
AmaanKhanaffy <amaan.khan@affyclouditsolutions.com>
2026-06-25 16:18:52 +0530

    New

--- frontend-app/dist/assets/BarChart-BKJhdJWw.js
+++
@@ -1,1 +0,0 @@
-import{l as a,B as i,n as s}from"./generateCategoricalChart-Bv2KXfGn.js";import{X as t,Y as r}from"./YAxis-CnlEdDlN.js";var p=a({chartName:"BarChart",GraphicalChild:i,defaultTooltipEventType:"axis",validateTooltipEventTypes:["axis","item"],axisComponents:[{axisType:"xAxis",AxisComp:t},{axisType:"yAxis",AxisComp:r}],formatAxisMap:s});export{p as B};
