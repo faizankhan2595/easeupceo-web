775c05eb2cf9fead01d33e6e709cc336a42b32d8
faizan Khan <faizankhan2595@gmail.com>
2026-06-30 00:18:37 +0530

    New

---
+++ frontend-app/dist/assets/minus-lLmRceot.js
@@ -0,0 +1,6 @@
+import{w as o}from"./index-Cw9VcDIb.js";/**
+ * @license lucide-react v0.507.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M5 12h14",key:"1ays0h"}]],e=o("minus",s);export{e as M};
